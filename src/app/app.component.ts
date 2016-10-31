68af31566128f84f860e318f1ea1a454575ba34d
mikesaprykin <mikesaprykin@gmail.com>
2016-10-31 12:58:37 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../assets/scss/index.scss';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
